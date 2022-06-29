998ea161a5ef6f4d959c501277a9a936db9ae213
samer <samervalente@gmail.com>
2022-06-29 18:02:57 -0300

    feat:register user and validate data

--- src/components/Login/Register.js
+++ src/components/Login/Register.js
@@ -1,6 +1,7 @@
 import {useEffect, useState} from "react"
 import { Link, useNavigate } from "react-router-dom"
-import {axios} from "axios"
+import axios from "axios"
+import { toBeInTheDocument } from "@testing-library/jest-dom/dist/matchers"
 
 export default function Register(){
     const [body, setBody] = useState({
@@ -9,11 +10,37 @@ export default function Register(){
         password:"",
     })
     const [confirm, setConfirm] = useState("")
+    const [validate, setValidate] = useState(false)
+
    
+    useEffect(() => {
+      
+        if(confirm === body.password && body.password !== ""){
+            setValidate(true)
+        }
+        if(confirm !== body.password){
+            setValidate(false)
+        }
+        
+    })
+
+    
+
     function Register(event){
         event.preventDefault();
+        const promise = axios.post("http://localhost:5000/register", body)
+        promise.then((response) => {
+            console.log(response.data)
+        }).catch((e) => {
+            console.log(e)
+            if(e.response.status === 422){
+                alert("Dados inválidos")
+            }
 
-        const promise = axios.post("http://localhost:5000/register")
+            if(e.response.status === 409){
+                alert("Usuário já registrado.")
+            }
+        })
 
     }
 
@@ -26,7 +53,16 @@ export default function Register(){
                 <input placeholder="E-mail" onChange={(e) => setBody({...body, email:e.target.value})}></input>
                 <input placeholder="Senha" onChange={(e) => setBody({...body, password:e.target.value})}></input>
                 <input placeholder="Confirme a senha" onChange={(e) => {setConfirm(e.target.value)}}></input>
-                <button>Cadastrar</button>
+               {validate ?  <button className="bg-violet-500" type="submit">Cadastrar</button> :  
+                <div className="w-full relative">
+                <button type="button" disabled className="bg-white opacity-80 text-violet-500">
+                    Senhas não conferem 
+                </button>
+                <ion-icon className="absolute left-0.5" name="alert-circle-outline"></ion-icon>
+                </div>
+                
+             
+               }
           </form>
            <Link to="/">
             <h3 className="text-white my-5 text-base">Já possui uma conta? Entre agora!</h3>
